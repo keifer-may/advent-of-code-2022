75c769a7f772450dbe2ad4344e88d95742174f1a
Keifer May <dkmay3@gmail.com>
2023-05-27 21:23:56 -0400

    Begin work on day 5, set up skeleton of structs and functions

---
+++ day_five/src/main.rs
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+struct Instructions {
+    num_objects: u16,
+    from_vec: &str,
+    to_vec: &str,
+}
+
+impl Instructions{
+
+    fn apply_instructions(Vec<Vec<char>>, self) -> Vec<<Vec<char>> {
+        
+    }
+
+
+
+
+
+}
+
+
+
+
+fn get_line_instructions(file_path: &str) -> Vec<Instructions> {
+
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let input_path = "./src/input.txt";
+
+    let list_instructions = get_line_instructions(input_path);
+
+
+}
